092744c9ba830e4ad4604679d0d1723f2086ba38
tholeb <43311443+tholeb@users.noreply.github.com>
2022-06-26 20:25:09 +0200

    Add: pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	apps: [
+		{
+			name: 'Dumbot',
+			script: 'npm',
+			args: 'start:prod',
+			watch: [
+				'src/**/*.js',
+				'main.js',
+			],
+			autorestart: true,
+			restart_delay: 3000,
+		},
+	],
+	deploy : {
+		production : {
+			'user' : 'pi',
+			'host' : '192.168.1.11',
+			'ref'  : 'origin/master',
+			'repo' : 'git@github.com:tholeb/dumbot.git',
+			'path' : '~/dumbot',
+			'post-deploy' : 'npm install',
+		},
+	},
+};
