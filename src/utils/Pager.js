40585aa527d56c4d32214b8dd591b58a1347fbac
tholeb <43311443+tholeb@users.noreply.github.com>
2022-07-07 20:16:12 +0200

    Add: Pager class

--- src/utils/Pager.js
+++ src/utils/Pager.js
@@ -1,20 +1,56 @@
 const { MessageActionRow } = require('discord.js');
 
-module.exports = (page, maxPage) => {
-    return new MessageActionRow().addComponents([
+module.exports = class Pager {
+    constructor(interaction, embeds, index) {
+        this.interaction = interaction;
+        this.embeds = embeds;
+        this.index = index;
+        this.length = embeds.length;
+    }
+
+
+    /**
+     * Run the pager collector
+     * @param  {} extraButtons if you want to add extra buttons
+     */
+    run(extraButtons) {
+        const collector = this.interaction.channel.createMessageComponentCollector({ time: 60000 });
+
+        collector.on('collect', async i => {
+            await i.deferUpdate();
+            switch (i.customId) {
+                case 'previous':
+                    this.index--;
+                    await this.interaction.editReply({ embeds: [this.embeds[this.index]], components: [this.makeButton()] });
+                break;
+                case 'next':
+                    this.index++;
+                    await this.interaction.editReply({ embeds: [this.embeds[this.index]], components: [this.makeButton()] });
+                break;
+                }
+        });
+
+        collector.on('end', collected => {
+            this.interaction.editReply({ content: `Délai d'attente dépassé (Vous avez tourné ${collected.length} pages).`, components: [extraButtons] });
+        });
+    }
+
+    makeButton() {
+        return new MessageActionRow().addComponents([
         {
             type: 'BUTTON',
             style: 'PRIMARY',
             customId: 'previous',
             label: '❮',
-            disabled: page == 0,
+            disabled: this.index == 0,
         },
         {
             type: 'BUTTON',
             style: 'PRIMARY',
             customId: 'next',
             label: '❯',
-            disabled : page == maxPage - 1,
+            disabled: this.index == this.length - 1,
         },
     ]);
+    }
 };
